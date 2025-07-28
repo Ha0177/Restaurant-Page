b5fc3ee60cb1a146fe26bf0721c44595fa0c7bc3
ha0177 <214249506+Ha0177@users.noreply.github.com>
2025-07-28 10:05:30 +0300

    Add initial implementation of index.js and style.css; set up basic layout and styles for the application

---
+++ src/DOM-module.js
@@ -0,0 +1,22 @@
+import restaurantImg from "./restaurant-img.png"
+
+export function loadPage() {
+    const container = document.querySelector("#content")
+    const headerImg = document.createElement("img");
+
+    const headline = document.createElement("h1");
+    headline.textContent = "Welcome to my restaurant!"
+    headline.classList.add("headline")
+
+    const description = document.createElement("p");
+    description.textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore itaque tempora magni molestias. Dolorem dolor totam tempora dolorum, corporis deleniti reiciendis rem, modi neque fugit maiores? Sunt ipsa architecto itaque."
+    description.classList.add("description")
+
+    headerImg.src = restaurantImg;
+    headerImg.classList.add("header-img");
+    headerImg.alt = "Restaurant Image";
+
+    container.appendChild(headerImg);
+    container.appendChild(headline);
+    container.appendChild(description);
+}
